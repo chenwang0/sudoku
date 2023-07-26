a9d52a7d9d5233d689572f4b41d095aaa9406371
chwein <chwein@126.com>
2023-07-26 13:27:24 +0800

    补提main分支

--- sudoku/src/main/java/com/sudoku/SudokuMain.java
+++ sudoku/src/main/java/com/sudoku/SudokuMain.java
@@ -2,16 +2,16 @@ package com.sudoku;
 
 public class SudokuMain {
 
-	public static void main(String[] args) {
-		int[][] sudoku = new int[9][9];
-		SudokuSolver sudokuSolver = new SudokuSolver(sudoku);
-		if (sudokuSolver.solve()) {
-			sudokuSolver.printSolution();
-		} else {
-			System.err.println("No solution found.");
-		}
+    public static void main(String[] args) {
+        int[][] sudoku = new int[9][9];
+        SudokuSolver sudokuSolver = new SudokuSolver(sudoku);
+        if (sudokuSolver.solve()) {
+            sudokuSolver.printSolution();
+        } else {
+            System.err.println("No solution found.");
+        }
 
-		sudokuSolver.sudoku[0][0] = 0;
-		System.out.println(sudokuSolver.isValid(0, 0, 1));
-	}
+        sudokuSolver.sudoku[0][0] = 0;
+        System.out.println(sudokuSolver.isValid(0, 0, 1));
+    }
 }
--- sudoku/src/main/java/com/sudoku/SudokuSolverEnhance.java
+++ sudoku/src/main/java/com/sudoku/SudokuSolverEnhance.java
@@ -7,64 +7,63 @@ import java.util.Random;
 
 public class SudokuSolverEnhance extends SudokuSolver {
 
-	private final Random rand = new Random();
+    private final Random rand = new Random();
 
-	public SudokuSolverEnhance(int[][] sudoku) {
-		super(sudoku);
-	}
+    public SudokuSolverEnhance(int[][] sudoku) {
+        super(sudoku);
+    }
 
-	public static SudokuSolverEnhance init() {
-		int[][] sudoku = new int[9][9];
-		return new SudokuSolverEnhance(sudoku);
-	}
+    public static SudokuSolverEnhance init() {
+        int[][] sudoku = new int[9][9];
+        return new SudokuSolverEnhance(sudoku);
+    }
 
-	// 生成初始模型
-	public SudokuSolverEnhance generateShape() {
-		List<Integer> tempList = new ArrayList<>(9);
+    // 生成初始模型
+    public SudokuSolverEnhance generateShape() {
+        List<Integer> tempList = new ArrayList<>(9);
 
-		// start generate the first row
-		for (int i = 1; i <= 9; i++) {
-			tempList.add(i);
-		}
-		Collections.shuffle(tempList);
-		for (int i = 0; i < 9; i++) {
-			super.sudoku[0][i] = tempList.get(i);
-		}
-		// complete generate
-		super.solve();
-		return this;
-	}
-
-
-	public SudokuSolverEnhance upset(int removeNum) {
-		int x = 0, y = 0;
-		for (int i = 0; i < removeNum; i++) {
-
-			do {
-				x = rand.nextInt(super.sudoku.length);
-				y = rand.nextInt(super.sudoku[x].length);
-			} while (super.sudoku[x][y] == 0);
-
-			// insure index is not `0`
-			super.sudoku[x][y] = 0;
-		}
-		return this;
-	}
-
-	// 刷新模型(每次求解)
-	public boolean refresh(int row, int col, int fillNum) {
-		return !super.isValid(row, col, fillNum) || (super.sudoku[row][col] != 0);
-	}
+        // start generate the first row
+        for (int i = 1; i <= 9; i++) {
+            tempList.add(i);
+        }
+        Collections.shuffle(tempList);
+        for (int i = 0; i < 9; i++) {
+            super.sudoku[0][i] = tempList.get(i);
+        }
+        // complete generate
+        super.solve();
+        return this;
+    }
 
 
+    public SudokuSolverEnhance upset(int removeNum) {
+        int x = 0, y = 0;
+        for (int i = 0; i < removeNum; i++) {
 
-	public static void main(String[] args) {
-		SudokuSolverEnhance sudokuSolverBuild = SudokuSolverEnhance.init().generateShape();
+            do {
+                x = rand.nextInt(super.sudoku.length);
+                y = rand.nextInt(super.sudoku[x].length);
+            } while (super.sudoku[x][y] == 0);
+
+            // insure index is not `0`
+            super.sudoku[x][y] = 0;
+        }
+        return this;
+    }
+
+    // 刷新模型(每次求解)
+    public boolean refresh(int row, int col, int fillNum) {
+        return !super.isValid(row, col, fillNum) || (super.sudoku[row][col] != 0);
+    }
+
+
+    public static void main(String[] args) {
+        SudokuSolverEnhance sudokuSolverBuild = SudokuSolverEnhance.init().generateShape();
 //		sudokuSolverBuild.printSolution();
-		sudokuSolverBuild.upset(0);
-		sudokuSolverBuild.printSolution();
-		System.out.println(sudokuSolverBuild.isValid(0, 0, 1));
+        sudokuSolverBuild.upset(0);
+        sudokuSolverBuild.printSolution();
+        System.out.println(sudokuSolverBuild.isValid(0, 0, 1));
 
-	}
+    }
 
 }
